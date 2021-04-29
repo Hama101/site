6b18a96766936accd610d03617d1d39a56770b17
hamdi Mohamed <hamdi_mohamed2018@hotmail.com>
2021-04-29 15:03:32 +0000

    dd

--- project/settings.py
+++ project/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -95,6 +95,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
